b960f7c43aaf335e1bc5f5016cbda1d94ac138f6
Frankstein97 <francoivannicoletti@gmail.com>
2023-04-13 00:41:13 -0300

    servidor con express entrega

--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 
-class ProductManager {
+export default class ProductManager {
   constructor() {
     this.patch = "./productos.txt";
     this.products = [];
@@ -63,10 +63,21 @@ await fs.writeFile(this.patch, JSON.stringify(update));
 }
 }
 
-const productos = new ProductManager();
+// const productos = new ProductManager();
 // productos.addProduct("title", "description", 1000, "img", "12abc", 10);
 // productos.addProduct("title2", "description2", 1000, "img2", "13abc", 10);
 // productos.addProduct("title3", "description3", 1000, "img3", "14abc", 10);
+
+// productos.addProduct("Seta de ostra", "Hongo comestible con textura delicada y sabor suave.", 5.99, "https://ejemplo.com/seta-ostra.jpg", "OSTR123", 50);
+// productos.addProduct("Champiñón portobello", "Hongo grande y carnoso con sabor terroso y ligeramente dulce.", 7.99, "https://ejemplo.com/portobello.jpg", "PORTO456", 30);
+// productos.addProduct("Reishi", "Hongo medicinal con propiedades antioxidantes y antiinflamatorias.", 12.99, "https://ejemplo.com/reishi.jpg", "REISH789", 20);
+// productos.addProduct("Shiitake", "Hongo aromático con sabor ahumado y notas terrosas.", 9.99, "https://ejemplo.com/shiitake.jpg", "SHII456", 40);
+// productos.addProduct("Maitake", "Hongo con forma de abanico y sabor suave y dulce.", 11.99, "https://ejemplo.com/maitake.jpg", "MAIT123", 25);
+// productos.addProduct("Boletus", "Hongo carnoso y sabroso con notas terrosas y amargas.", 14.99, "https://ejemplo.com/boletus.jpg", "BOLE789", 15);
+// productos.addProduct("Pleurotus", "Hongo comestible de sabor suave y textura delicada.", 6.99, "https://ejemplo.com/pleurotus.jpg", "PLEU456", 35);
+// productos.addProduct("Amanita muscaria", "Hongo tóxico con aspecto distintivo de sombrero rojo y puntos blancos.", 0.99, "https://ejemplo.com/amanita.jpg", "AMAN123", 5);
+// productos.addProduct("Trufa blanca", "Hongo subterráneo muy apreciado por su aroma y sabor únicos.", 29.99, "https://ejemplo.com/trufa-blanca.jpg", "TRUF789", 10);
+// productos.addProduct("Hongo del pie azul", "Hongo alucinógeno con aspecto de sombrero convexo y pie azulado.", 9.99, "https://ejemplo.com/hongo-azul.jpg", "AZUL456", 2);
 
 // productos.getProducts()
 
